8d1ffd92f14f17f1faf408ee6b75780222e63157
Juan <juanmarten99@gmail.com>
2019-09-11 14:23:30 -0300

    Rutas en src GUI

--- TowerDefense/src/GUI.java
+++ TowerDefense/src/GUI.java
@@ -41,17 +41,17 @@ public class GUI extends JFrame {
 		String[] rutas2= {"AtaqueAlien.gif","AtaqueDragon.gif","AtaqueFantasma.gif","AtaqueGolem.gif","AtaqueHada.gif"};
 		ImageIcon image;
 		for(int i=0;i<7;i++) {
-			image=new ImageIcon(this.getClass().getResource("/Sprites/Torres/"+rutas1[i]));
+			image=new ImageIcon("./src/Sprites/Torres/"+rutas1[i]);
 			dibujo[i]=new JLabel(image);
 			dibujo[i].setBounds(0,i*96,96,96);
 			this.add(dibujo[i]);
 		}
-		dibujo[6]=new JLabel(new ImageIcon(this.getClass().getResource("/Sprites/Torres/"+rutas1[6])));
+		dibujo[6]=new JLabel(new ImageIcon("./src/Sprites/Torres/"+rutas1[6]));
 		dibujo[6].setBounds(96,5*96,96,96);
 		this.add(dibujo[6]);
 		disparos= new JLabel[5];
 		for(int i=0;i<5;i++) {
-			image=new ImageIcon(this.getClass().getResource("/Sprites/Ataques/"+rutas2[i]));
+			image=new ImageIcon("./src/Sprites/Ataques/"+rutas2[i]);
 			disparos[i]=new JLabel(image);
 			disparos[i].setBounds(96,i*96,96,96);
 			this.add(disparos[i]);
