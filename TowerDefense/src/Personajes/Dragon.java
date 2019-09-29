74a7f7f63439faef68527469c892f9a6a5e3b61a
CarolinaSiracusa <54592916+CarolinaSiracusa@users.noreply.github.com>
2019-09-28 22:58:22 -0300

    Enemigos y Torres gifs

--- TowerDefense/src/Personajes/Dragon.java
+++ TowerDefense/src/Personajes/Dragon.java
@@ -8,7 +8,7 @@ public class Dragon extends Torre {
 	protected static int vidaBase=600;
 	protected static int danoBase=50;
 	protected static int alcanceBase=7;
-	protected static String imagen ="./src/Sprites/Torres/Dragon.png";
+	protected static String imagen ="./src/Sprites/Torres/Dragon.gif";
 	protected static String proyectil="./src/Sprites/Ataques/AtaqueDragon.gif";
 	
 	public Dragon(Mapa mapa,Celda celda) {
--- TowerDefense/src/Personajes/Enemigo5.java
+++ TowerDefense/src/Personajes/Enemigo5.java
@@ -7,7 +7,7 @@ public class Enemigo5 extends Enemigo {
 	protected static int vidaBase=500;
 	protected static int danoBase=70;
 	protected static int alcanceBase=6;
-	protected static String imagen ="./src/Sprites/Enemigos/Enemigo5b.gif";
+	protected static String imagen ="./src/Sprites/Enemigos/Enemigo5.gif";
 	protected static String proyectil="./src/Sprites/Ataques/Enemigo5.gif";
 	
 	public Enemigo5(Mapa mapa,Celda celda) {
