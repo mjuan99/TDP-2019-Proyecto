106b4fff99e7b50b35d85d11c0bee98909df004d
Juan <juanmarten99@gmail.com>
2019-10-24 23:55:25 -0300

    Emprolijamiento

--- TowerDefense/src/Personajes/Leviatan.java
+++ TowerDefense/src/Personajes/Leviatan.java
@@ -15,7 +15,7 @@ public class Leviatan extends Torre {
 	protected static String imagen ="./src/Sprites/Torres/Onix.gif";
 	protected static String proyectil="";
 	protected static String nomb="Leviatan";
-	protected static String mini="./src/Sprites/Tienda/Leviatana.png";
+	protected static String mini="./src/Sprites/Tienda/Onix.png";
 	
 	public Leviatan(Celda celda) {
 		super(celda,vidaBase,tam,imagen,danoBase,alcanceBase,proyectil,precio,mini,nomb);
--- TowerDefense/src/Tienda/TorreTienda.java
+++
@@ -1,9 +0,0 @@
-package Tienda;
-
-import Personajes.Torre;
-
-public abstract class TorreTienda {
-	public abstract Torre getTorre();
-	public abstract String getImagen();
-	public abstract String descripcion();
-}
