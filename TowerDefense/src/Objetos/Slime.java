1b8ec6b93afe6e5cfd8812e1e5d235ad528f79fc
Juan <juanmarten99@gmail.com>
2019-10-31 13:56:14 -0300

    Obstaculos

--- TowerDefense/src/Objetos/Slime.java
+++ TowerDefense/src/Objetos/Slime.java
@@ -6,7 +6,7 @@ public class Slime extends ObstaculoTiempo{
 	protected static int vidaBase=600;
 	protected static int tam=1;
 	protected static String imagen ="./src/Sprites/Obstaculos/Slime.gif";
-	protected static int tiempo=500;
+	protected static int tiempo=50;
 	protected static String nombre="Slime baboso";
 	protected static String mini="";
 	protected static int precio=75;
--- TowerDefense/src/Objetos/Topo.java
+++ TowerDefense/src/Objetos/Topo.java
@@ -6,7 +6,7 @@ public class Topo extends ObstaculoTiempo {
 	protected static int vidaBase=50;
 	protected static int tam=1;
 	protected static String imagen="./src/Sprites/Obstaculos/Topo.gif";
-	protected static int tiempo=500;
+	protected static int tiempo=50;
 	protected static String mini="";
 	protected static String nombre= "Topo";
 	protected static int precio=50;
