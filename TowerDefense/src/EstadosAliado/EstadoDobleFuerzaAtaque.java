71227775255407ebdc746b47b96c2d8c0c2c23fb
CarolinaSiracusa <54592916+CarolinaSiracusa@users.noreply.github.com>
2019-11-01 21:39:25 -0300

    asdfghj

--- TowerDefense/src/EstadosAliado/EstadoDobleFuerzaAtaque.java
+++ TowerDefense/src/EstadosAliado/EstadoDobleFuerzaAtaque.java
@@ -1,5 +1,8 @@
 package EstadosAliado;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
 import Juego.Mapa;
 import Objetos.ProyectilAliado;
 import Personajes.Torre;
@@ -8,16 +11,21 @@ public class EstadoDobleFuerzaAtaque implements EstadoAtaqueAliado {
 	protected Torre miTorre;
 	protected long inicio;
 	protected int duracion=7000;
+	JLabel componente= new JLabel(new ImageIcon("./src/Sprites/Efectos/DobleFuerza/DobleFuerza.gif"));
 	public EstadoDobleFuerzaAtaque(Torre t) {
 		miTorre=t;
+		componente.setBounds(0, 0, 96, 96);
+		miTorre.getComponenteGrafica().add(componente);
 		inicio=System.currentTimeMillis();
 	}
 	
 	public void atacar() {
 		if(System.currentTimeMillis()<inicio+duracion) {
 			Mapa.getMapa().crearElementoIntangible(new ProyectilAliado(miTorre.getCelda(),miTorre , 2*miTorre.getDano(),miTorre.getAlcance(),miTorre.getRutaProyectil()));
+		
 		}
 		else {
+			miTorre.getComponenteGrafica().remove(componente);
 			miTorre.setEstadoAtaque(new EstadoDefaultAtaque(miTorre));
 			miTorre.atacar();
 		}
